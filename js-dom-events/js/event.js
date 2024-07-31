f5105aebb72d4bfb9d886c8d7122367d7a49317e
ymofficial <ymofficial2023@gmail.com>
2024-08-01 03:01:50 +0600

    dom-events

---
+++ js-dom-events/js/event.js
@@ -0,0 +1,11 @@
+console.log('This is separate JS file');
+
+// function for onclick green ...
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+//function for darkmode ...
+function darkMode(){
+    document.body.style.backgroundColor ='#0f172a';
+}
